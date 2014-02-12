fe5a0f3064a562cf1678deb50994334b3e969f08
David Saltzman <dsaltz@stanford.edu>
2014-02-12 21:37:32 +0000

    did lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,21 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, addProject);
+	console.log("User click: getting /project/" + idNumber);
+}
+
+function addProject(result) {
+	console.log(result);
+
+	// $("#project" + result.id + " .details").html("foo");
+
+	var projectHTML = '<img src="' + result.image + '" class="detailsImage">' +
+					  '<h3>' + result.title + '</h3>' +
+					  '<p><small>' + result.date + '</small></p>' +
+					  '<p>' + result.summary + '</p>';
+
+    $("#project" + result.id + " .details").html(projectHTML);
 }
 
 /*
@@ -35,4 +49,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", setColors);
+}
+
+function setColors(result) {
+	var colors = result.colors.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
